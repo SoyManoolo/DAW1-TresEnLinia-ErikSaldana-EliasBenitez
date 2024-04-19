d6f5b8740924147591ed4d65d10d10ef5c592806
soymanolo <esaldana@elpuig.xeill.net>
2024-04-20 00:56:42 +0200

    respuesta de las opciones del menú

---
+++ src/TUI.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Locale;
+public class TUI {
+
+    public static void opcionesMenu() {
+
+        Scanner sc =new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
+
+        int eleccion_menu = sc.nextInt();
+        switch (eleccion_menu) {
+            case 1:
+                System.out.println("Has iniciado una nueva partida.");
+                break;
+            case 2:
+                System.out.println("Has cargado la partida guardada.");
+                break;
+            case 3:
+                System.out.println("Has abierto la configuración.");
+                break;
+            case 4:
+                System.out.println("Has salido del tres en raya");
+                break;
+        }
+    }
+}
