f07b0e39d39bb310407a439ec02333a64eb4c3cc
soymanolo <esaldana@elpuig.xeill.net>
2024-04-25 19:38:42 +0200

    He quitado el código escrito en la clase Main y Juego

--- src/Juego.java
+++ src/Juego.java
@@ -1,24 +1,4 @@
-import java.util.Scanner;
 
 public class Juego {
-    private String [][] enRaya = new String[3][3];
 
-    public void enPartida (String [][] matriz) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Comienza la 'X'.");
-        System.out.println("Introduce el espacio donde quieres hacer tu jugada.");
-        boolean victoria = false;
-
-        while (victoria == false) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-
-            if (matriz[x][y] == "X" || matriz[x][y] == "O")
-                System.out.println("Esta casilla ya ha sido rellenada. Elige otra casilla.");
-            else {
-                matriz[x][y] = sc.nextLine();
-            }
-        }
-    }
 }
