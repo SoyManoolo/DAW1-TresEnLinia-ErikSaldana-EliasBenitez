dcbce6035fa2290a49b4c034d548a8f1bd7153be
soymanolo <esaldana@elpuig.xeill.net>
2024-05-02 20:13:22 +0200

    Actualizar rama Manolo en el remoto

---
+++ src/JuegoTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class JuegoTest {
+
+    @org.junit.jupiter.api.Test
+    void novaPartida() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void jugar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void jugadaGuanyadora() {
+    }
+}
