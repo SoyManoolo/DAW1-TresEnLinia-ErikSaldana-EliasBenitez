cc3cd8ae2a1f341f473b77a6bdf60348c83ab944
soymanolo <esaldana@elpuig.xeill.net>
2024-05-06 19:24:50 +0200

    He añadido el maven

--- src/main/java/JuegoTest.java
+++ src/main/java/JuegoTest.java
@@ -1,9 +1,12 @@
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class JuegoTest {
 
     @org.junit.jupiter.api.Test
     void novaPartida() {
+
     }
 
     @org.junit.jupiter.api.Test
