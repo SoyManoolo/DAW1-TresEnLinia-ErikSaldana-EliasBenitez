b45e137b8d70baec4191b480a283e6123114bcd9
soymanolo <esaldana@elpuig.xeill.net>
2024-05-06 11:59:51 +0200

    He añadido los metodos de la clase Main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,21 @@
 public class Main {
+    public void novaPartida() {
+
+    }
+
+    public void carregarPartida() {
+
+    }
+
+    public void configuracio() {
+
+    }
+
+    public void sortir() {
+
+    }
+
+
     public static void main(String[] args) {
 
     }
