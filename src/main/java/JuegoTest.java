58da8acfe8f8f1a7a50dc6b0b6184edf51728979
soymanolo <esaldana@elpuig.xeill.net>
2024-05-07 03:08:54 +0200

    He escrito el codigo de los metodos novaPartida() y jugar() y sus respectivas pruebas unitarias.

--- src/main/java/JuegoTest.java
+++ src/main/java/JuegoTest.java
@@ -6,11 +6,17 @@ class JuegoTest {
 
     @org.junit.jupiter.api.Test
     void novaPartida() {
-
+        Juego j = new Juego();
+        String [] [] matriu = j.novaPartida();
+        assertEquals(3, matriu.length );
     }
 
     @org.junit.jupiter.api.Test
     void jugar() {
+        String [][] taulell = new String[3][3];
+        Juego j = new Juego();
+        String [] [] valor = j.jugar(taulell);
+        assertEquals("X", valor [1] [2]);
     }
 
     @org.junit.jupiter.api.Test
