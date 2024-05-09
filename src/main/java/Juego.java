fcaf5e2d08f0ff23e94595c101f07a413c49124d
soymanolo <esaldana@elpuig.xeill.net>
2024-05-09 20:02:30 +0200

    He añadido que se revise la jugada ganadora tambien para el jugador 2, esto dependerá de quien sea el turno.

--- src/main/java/Juego.java
+++ src/main/java/Juego.java
@@ -33,25 +33,50 @@ public class Juego {
 
     public boolean jugadaGuanyadora(int fila, int columna) {
         boolean jugada = false;
-        taulell[fila][columna] = 'X';
 
-        for (int i = 0; i < taulell.length; i++) { //bucle que dice si hay alguna jugada ganadora en horizontal
-            if (taulell[i][0] == 'X' && taulell[i][1] == 'X' && taulell[i][2] == 'X') {
+        if (torn == 1){
+            taulell[fila][columna] = 'X';
+
+            for (int i = 0; i < taulell.length; i++) { //bucle que dice si hay alguna jugada ganadora en horizontal para el jugador 1
+                if (taulell[i][0] == 'X' && taulell[i][1] == 'X' && taulell[i][2] == 'X') {
+                    jugada = true;
+                }
+            }
+
+            for (int i = 0; i < taulell.length; i++) { //bucle que dice si hay alguna jugada ganadora en vertical para el jugador 1
+                if (taulell[0][i] == 'X' && taulell[1][i] == 'X' && taulell[2][i] == 'X') {
+                    jugada = true;
+                }
+            }
+
+            if (taulell[0][0] == 'X' && taulell[1][1] == 'X' && taulell[2][2] == 'X'){ //comprueba si hay jugada ganadora en las diagonales
                 jugada = true;
             }
-        }
-
-        for (int i = 0; i < taulell.length; i++) { //bucle que dice si hay alguna jugada ganadora en vertical
-            if (taulell[0][i] == 'X' && taulell[1][i] == 'X' && taulell[2][i] == 'X') {
+            else if (taulell[0][2] == 'X' && taulell[1][1] == 'X' && taulell[2][0] == 'X'){
                 jugada = true;
             }
-        }
 
-        if (taulell[0][0] == 'X' && taulell[1][1] == 'X' && taulell[2][2] == 'X'){
-            jugada = true;
-        }
-        else if (taulell[0][2] == 'X' && taulell[1][1] == 'X' && taulell[2][0] == 'X'){
-            jugada = true;
+        } else if (torn == 2) {
+            taulell[fila][columna] = 'O';
+
+            for (int i = 0; i < taulell.length; i++) { //bucle que dice si hay alguna jugada ganadora en horizontal para el jugador 2
+                if (taulell[i][0] == 'O' && taulell[i][1] == 'O' && taulell[i][2] == 'O') {
+                    jugada = true;
+                }
+            }
+
+            for (int i = 0; i < taulell.length; i++) { //bucle que dice si hay alguna jugada ganadora en vertical para el jugador 2
+                if (taulell[0][i] == 'O' && taulell[1][i] == 'O' && taulell[2][i] == 'O') {
+                    jugada = true;
+                }
+            }
+
+            if (taulell[0][0] == 'O' && taulell[1][1] == 'O' && taulell[2][2] == 'O'){ //comprueba si hay jugada ganadora en las diagonales
+                jugada = true;
+            }
+            else if (taulell[0][2] == 'O' && taulell[1][1] == 'O' && taulell[2][0] == 'O'){
+                jugada = true;
+            }
         }
 
         taulell[2][2] = '\u0000';
